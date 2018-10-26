056e94933773e62cc096491bc709a37124690411
sansajn <mudrinic5n@outlook.com>
2018-10-26 10:10:04 +0200

    fixed swagger

--- src/main/java/nucleus/it/orianna/configuration/swagger/SwaggerConfiguration.java
+++ src/main/java/nucleus/it/orianna/configuration/swagger/SwaggerConfiguration.java
@@ -2,16 +2,20 @@ package nucleus.it.orianna.configuration.swagger;
 
 import nucleus.it.orianna.configuration.ApplicationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger configuration
  * @author sansajn
  */
+@Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
 
     private final ApplicationProperties applicationProperties;
